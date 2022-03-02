6bb3638e942259b1346b9bcf787341754facf147
Joshua <jrktempla@gmail.com>
2022-03-02 18:13:25 +0800

    Setting up bare bones
    Changed default db from sqlite to mysql
    
    please create new db named dbRedwood

--- Redwood/settings.py
+++ Redwood/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Main'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,24 @@ WSGI_APPLICATION = 'Redwood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbRedwood',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command': "SET sql_mode= 'STRICT_TRANS_TABLES'",
+        },
     }
 }
 
