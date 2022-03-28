513453cae6402497aa23811335fbc23b922c6293
Ivan <55166879+ivananoosGithub@users.noreply.github.com>
2022-03-28 14:30:27 +0800

    final

--- Redwood/settings.py
+++ Redwood/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f+ni$rwaa@v2ah$fh7l1ra87-!%f#r&h^kr$t50$@ac5rl^-4y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://molina-my-first-azure-deployed-app.azurewebsites.net','molina-my-first-azure-deployed-app.azurewebsites.net','*','localhost','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -46,7 +46,6 @@ STATIC_URL = '/static/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -89,14 +88,14 @@ WSGI_APPLICATION = 'Redwood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'it342-dbstudent-molina',
-        'USER': 'MonaMolina',
-        'PASSWORD': '103101Moniel*22',
-        'HOST': 'it342-dbstudent-molina.postgres.database.azure.com',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbRedwood',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
-            "sslmode": "required",
+            'init_command': "SET sql_mode= 'STRICT_TRANS_TABLES'",
         },
     }
 }
@@ -139,7 +138,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
